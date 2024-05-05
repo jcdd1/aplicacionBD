dda1e820de0f94e213f9b580dd1ee18cfae9a976
jcdd1 <cami-516@hotmail.com>
2024-05-05 08:14:40 -0500

    Update file Program

--- aplicacionBD/Program.cs
+++ aplicacionBD/Program.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Windows.Forms;
 namespace aplicacionBD
 {
     internal static class Program
@@ -10,7 +12,8 @@ namespace aplicacionBD
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
